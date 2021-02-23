b379ebab968445162e0463c950cd7a37a1b71c35
liamtw22 <31185529+liamtw22@users.noreply.github.com>
2021-02-23 16:53:38 -0500

    Havoc-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rs988.mk
+    $(LOCAL_DIR)/havoc_rs988.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rs988-userdebug \
-    lineage_rs988-eng
+    havoc_rs988-userdebug \
+    havoc_rs988-eng
--- havoc_rs988.mk
+++ havoc_rs988.mk
@@ -18,15 +18,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit from rs988 device
 $(call inherit-product, device/lge/rs988/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := rs988
-PRODUCT_NAME := lineage_rs988
+PRODUCT_NAME := havoc_rs988
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-RS988
 PRODUCT_MANUFACTURER := LGE
